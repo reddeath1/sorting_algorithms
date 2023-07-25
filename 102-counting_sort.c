8813e4cd2d64bb39af3649d9ec0535cc11b1f7fc
Reddeath <frankslayer1@gmail.com>
2023-07-26 01:43:10 +0300

    count

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,50 +1,52 @@
-#include "sorts.h"
+#include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-  * counting_sort - Main Afunction that sort an array using counting algorithm.
-  * @array: An array to sort.
-  * @size: An length of the array.
+  * counting_sort - main function that sort an array using counting algorithm.
+  * @array: The array to sort.
+  * @size: The length of the array.
   * Return: (Nothing).
   */
 void counting_sort(int *array, size_t size)
 {
 	unsigned int i = 1;
-	int *count = NULL, c = 0, j = 0;
+	int *c = NULL, k = 0, j = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
-	c = array[0];
+	k = array[0];
 	for (; i < size; i++)
 	{
 		if (array[i] > k)
-			c = array[i];
+			k = array[i];
 	}
 
-	count = malloc(sizeof(int) * (k + 1));
-	if (count == NULL)
+	c = malloc(sizeof(int) * (k + 1));
+	if (c == NULL)
 		return;
 
-	for (j = 0; j <= c; j++)
-		count[j] = 0;
+	for (j = 0; j <= k; j++)
+		c[j] = 0;
 	for (i = 0; i < size; i++)
-		count[array[i]] += 1;
-	for (j = 0; j < c; j++)
+		c[array[i]] += 1;
+	for (j = 0; j < k; j++)
 	{
-		count[j + 1] += count[j];
-		printf("%d, ", count[j]);
+		c[j + 1] += c[j];
+		printf("%d, ", c[j]);
 	}
-	count[j + 1] += count[j];
-	printf("%d\n", count[j + 1]);
+	c[j + 1] += c[j];
+	printf("%d\n", c[j + 1]);
 	for (i = 0; i < size; i++)
 	{
-		j = count[array[i]] - 1;
+		j = c[array[i]] - 1;
 		if (array[i] != array[j])
 		{
-			c = array[i];
+			k = array[i];
 			array[i] = array[j];
-			array[j] = c;
+			array[j] = k;
 		}
 	}
-	free(count);
+	free(c);
 }
